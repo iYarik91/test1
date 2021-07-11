37efb60f63ec4668c9ff5b7b48fc43c922e1fcf6
iYarik91 <iyarik91@gmail.com>
2021-07-11 19:03:50 +0000

    added sg inboung ports 80 443 22 5000

--- main.tf
+++ main.tf
@@ -14,6 +14,42 @@ provider "aws" {
     region  = "eu-central-1"
 }
 
+esource "aws_security_group_rule" "port443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["172.31.0.0/16"]
+  security_group_id = "sg-9beccaee"
+}
+
+resource "aws_security_group_rule" "port80" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["172.31.0.0/16"]
+  security_group_id = "sg-9beccaee"
+}
+
+resource "aws_security_group_rule" "port5000" {
+  type              = "ingress"
+  from_port         = 5000
+  to_port           = 5000
+  protocol          = "tcp"
+  cidr_blocks       = ["172.31.0.0/16"]
+  security_group_id = "sg-9beccaee"
+}
+
+resource "aws_security_group_rule" "port22" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["172.31.0.0/16"]
+  security_group_id = "sg-9beccaee"
+}
+
 resource "aws_instance" "app_server" {
     ami           = "ami-05f7491af5eef733a"
     instance_type = "t2.micro"
